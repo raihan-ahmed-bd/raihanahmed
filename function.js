52ef322b61766ea05bfdbb5facb34152f2a899c0
Ahmed Raihan <152580161+ahmed-raihan-bd@users.noreply.github.com>
2024-01-28 23:03:08 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+function menuOn() {
+  let menuOn = document.getElementById("nav-buttons");
+    menuOn.style.transform = "scaleY(1)";
+}
+function menuOff() {
+  let menuOff = document.getElementById("nav-buttons");
+    menuOff.style.transform = "scaleY(0)";
+}
