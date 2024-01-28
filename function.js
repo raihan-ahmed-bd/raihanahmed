2acc4b2875740a2c237d39b3ed39769428aa8d93
Ahmed Raihan <152580161+ahmed-raihan-bd@users.noreply.github.com>
2024-01-28 23:20:16 +0600

    function.js

--- function.js
+++
@@ -1,8 +0,0 @@
-function menuOn() {
-  let menuOn = document.getElementById("nav-buttons");
-    menuOn.style.transform = "scaleY(1)";
-}
-function menuOff() {
-  let menuOff = document.getElementById("nav-buttons");
-    menuOff.style.transform = "scaleY(0)";
-}
