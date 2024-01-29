5c52402cc3ea85b73006f5aabb6fde57e201654f
Ahmed Raihan <152580161+ahmed-raihan-bd@users.noreply.github.com>
2024-01-30 00:55:59 +0600

    Update function.js

--- function.js
+++ function.js
@@ -6,3 +6,10 @@ function menuOff() {
   let menuOff = document.getElementById("nav-buttons");
     menuOff.style.transform = "scaleY(0)";
 }
+function loading() {
+  let loading = document.getElementById("loading");
+    loading.style.opacity = 0;
+  setTimeout(() => {
+    loading.style.display = "none";
+}, 500);
+}
