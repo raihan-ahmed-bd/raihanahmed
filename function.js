d94bb14d1c165a143952b725e23ced0fa0594aea
Ahmed Raihan <152580161+ahmed-raihan-bd@users.noreply.github.com>
2024-01-30 15:32:23 +0600

    Update function.js

--- function.js
+++ function.js
@@ -17,7 +17,7 @@ function loading() {
       }, 500);
       loadingStopped = true;
     }
-  }, 2500);
+  }, 3000);
   window.addEventListener('load', () => {
     if (!loadingStopped) {
       preloader.style.opacity = 0;
